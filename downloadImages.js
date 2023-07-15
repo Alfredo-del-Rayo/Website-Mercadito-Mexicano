b0f7ff59f3b49aa126ae91f4c4e53585d5e776e9
Alfredo-del-Rayo <99233341+Alfredo-del-Rayo@users.noreply.github.com>
2023-07-14 19:12:08 -0700

    Initial

---
+++ downloadImages.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const axios = require('axios');
+const csv = require('csv-parser');
+
+const csvFilePath = 'products.csv';
+const downloadPath = 'images/';
+// Read the CSV file
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (row) => {
+    const imageSrc = row['Image Src']; // Replace with the actual column name from your CSV
+    const fileNameWithQuery = imageSrc.substring(imageSrc.lastIndexOf('/') + 1);
+    const fileName = fileNameWithQuery.split('?')[0];
+    const imagePath = `${downloadPath}${fileName}`; // Define the target path for saving the image
+    console.log(`${downloadPath}${fileName}`)
+    console.log(' Break Here ')
+    console.log(fileName)
+    console.log(' Break Here ')
+    axios
+      .get(imageSrc, { responseType: 'stream' })
+      .then((response) => {
+        response.data.pipe(fs.createWriteStream(imagePath));
+      })
+      .catch((error) => {
+        console.error(`Failed to download image: ${imageSrc}`);
+        console.error(error);
+      });
+  })
+  .on('end', () => {
+    console.log('Image download completed!');
+  });
